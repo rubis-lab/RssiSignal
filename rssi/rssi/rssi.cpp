33883dab12b4c157ce92b793394e52f19d1a9358
Jihwa Lee <shuride@gmail.com>
2015-12-09 23:18:01 +0900

    WlanScan period considerations

--- rssi/rssi/rssi.cpp
+++ rssi/rssi/rssi.cpp
@@ -12,6 +12,9 @@
 
 int main(void)
 {
+	FILE* fp = NULL;
+	fp = fopen("test.txt", "w");
+	
 	HANDLE hClient = NULL;
 	DWORD dwMaxClient = 2;
 	DWORD dwCurVersion = 0;
@@ -21,8 +24,7 @@ int main(void)
 
 	WCHAR GuidString[39] = { 0 };
 		
-	PWLAN_INTERFACE_INFO_LIST pIfList = NULL;
-	PWLAN_INTERFACE_INFO pIfConnInfo = NULL;
+	PWLAN_INTERFACE_INFO_LIST pIfList = NULL;	
 	PWLAN_INTERFACE_INFO pIfInfo = NULL;
 	PWLAN_BSS_LIST pBssList = NULL;
 	PWLAN_BSS_ENTRY pBssEntry = NULL;	
@@ -38,143 +40,128 @@ int main(void)
 	DWORD dwSizeRssi = sizeof(rssi);
 
 	dwResult = WlanOpenHandle(dwMaxClient, NULL, &dwCurVersion, &hClient);
-	if(dwResult != ERROR_SUCCESS)
-	{
-		wprintf(L"WlanOpenHandle failed with error: %u\n", dwResult);
-		getchar();
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanOpenHandle failed with error: %u\n", dwResult);		
 		return 1;		
 	}
 
 	dwResult = WlanEnumInterfaces(hClient, NULL, &pIfList);
-	if(dwResult != ERROR_SUCCESS)
-	{
-		wprintf(L"WlanEnumInterfaces failed with error: %u\n", dwResult);
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanEnumInterfaces failed with error: %u\n", dwResult);
 		return 1;		
 	}
 	else
 	{		
-		for(int i = 0; i < (int) pIfList->dwNumberOfItems; i++) {
+		for (int i = 0; i < (int) pIfList->dwNumberOfItems; i++) {
 			pIfInfo = (WLAN_INTERFACE_INFO *) & pIfList->InterfaceInfo[i];			
 			iRet = StringFromGUID2(pIfInfo->InterfaceGuid, (LPOLESTR) & GuidString, sizeof(GuidString) / sizeof(*GuidString));			
-			if(iRet == 0)
-			{
-				wprintf(L"StringFromGUID2 failed\n");
+			if (iRet == 0) {
+				printf("StringFromGUID2 failed\n");
+			}			
+		}
+		
+		while(1) {
+			// Query the Interface
+			dwResult = WlanQueryInterface(hClient,
+				&pIfInfo->InterfaceGuid,
+				wlan_intf_opcode_current_connection,					
+				NULL,
+				&connectInfoSize,
+				(PVOID *) &pConnectInfo,					
+				&opCode);
+			if (dwResult != ERROR_SUCCESS) {
+				printf("WlanQueryInterface failed with error: %u\n", dwResult);
+				return 0;
 			}
-			
-			if(pIfInfo->isState == wlan_interface_state_connected)
-			{
-				pIfConnInfo = pIfInfo;
-				break;
+
+			// Scan the connected SSID
+			dwResult = WlanScan(hClient,
+				&pIfInfo->InterfaceGuid,
+				&pConnectInfo->wlanAssociationAttributes.dot11Ssid,
+				NULL,
+				NULL);
+			if (dwResult != ERROR_SUCCESS) {
+				printf("WlanScan failed with error: %u\n", dwResult);
+				return 0;
+			}		
+
+			dwResult = WlanGetAvailableNetworkList(hClient,
+				&pIfInfo->InterfaceGuid,
+				0,
+				NULL,
+				&pAvaList);
+			if (dwResult != ERROR_SUCCESS) {
+				printf("WlanGetAvailableNetworkList failed with error: %u\n", dwResult);
+				return 0;
 			}
-		}
 
-		if(pIfConnInfo == NULL)
-		{
-			return 0;
-		}
-
-		// Query the Interface
-		dwResult = WlanQueryInterface(hClient,
-			&pIfInfo->InterfaceGuid,
-			wlan_intf_opcode_current_connection,					
-			NULL,
-			&connectInfoSize,
-			(PVOID *) &pConnectInfo,					
-			&opCode);
-
-		if(dwResult != ERROR_SUCCESS)
-		{
-			wprintf(L"WlanQueryInterface failed with error: %u\n", dwResult);
-			return 0;
-		}
-
-		// Scan the connected SSID
-		dwResult = WlanScan(hClient,
-			&pIfConnInfo->InterfaceGuid,
-			&pConnectInfo->wlanAssociationAttributes.dot11Ssid,
-			NULL,
-			NULL);
-		if(dwResult != ERROR_SUCCESS)
-		{
-			return 0;
-		}
-
-		dwResult = WlanGetAvailableNetworkList(hClient,
-			&pIfConnInfo->InterfaceGuid,
-			0,
-			NULL,
-			&pAvaList);
-		if(dwResult != ERROR_SUCCESS)
-		{
-			return 0;
-		}
-
-		wprintf(L"Num Entries : %lu\n", pAvaList->dwNumberOfItems);
-
-		for(int j = 0; j < pAvaList->dwNumberOfItems; j++)
-		{
-			pAvaEntry = (WLAN_AVAILABLE_NETWORK *) & pAvaList->Network[j];
-			wprintf(L"SSID[%u] : ", j);
-			if(pAvaEntry->dot11Ssid.uSSIDLength == 0)
+			printf("Number of APs : %lu\n", pAvaList->dwNumberOfItems);
+			fprintf(fp, "Number of APs : %lu\n", pAvaList->dwNumberOfItems);
+			fflush(fp);
+			for(int j = 0; j < pAvaList->dwNumberOfItems; j++)
 			{
-				wprintf(L"\n");
-			}
-			else
-			{   
+				pAvaEntry = (WLAN_AVAILABLE_NETWORK *) & pAvaList->Network[j];
 				for(int k = 0; k < pAvaEntry->dot11Ssid.uSSIDLength; k++)
 				{
-					wprintf(L"%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
+					printf("%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
+					fprintf(fp, "%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
+					fflush(fp);
 				}
-				wprintf(L"\n");
-			}	
+				printf("\t");
+				fprintf(fp, "\t");
+				fflush(fp);
 
-			// Get the BSS Entry		
-			dwResult = WlanGetNetworkBssList(hClient,
-				&pIfConnInfo->InterfaceGuid,
-				&pAvaEntry->dot11Ssid,
-				dot11_BSS_type_infrastructure,
-				TRUE,
-				NULL,
-				&pBssList);
+				dwResult = WlanGetNetworkBssList(hClient,
+					&pIfInfo->InterfaceGuid,
+					&pAvaEntry->dot11Ssid,
+					dot11_BSS_type_infrastructure,
+					TRUE,
+					NULL,
+					&pBssList);
+				if (dwResult != ERROR_SUCCESS) {
+					printf("WlanGetNetworkBssList failed with error: %u\n", dwResult);
+					return 0;
+				}				
 
-			if(dwResult != ERROR_SUCCESS)
-			{
-				return 0;
-			}				
-
-			// Get the RSSI value			
-			if(pBssList->dwNumberOfItems > 0)
-			{
-				pBssEntry = &pBssList->wlanBssEntries[0];			
-				wprintf(L"RSSI : %d dBm\n\n", pBssEntry->lRssi);
+				// Get the RSSI value			
+				if (pBssList->dwNumberOfItems > 0) {
+					pBssEntry = &pBssList->wlanBssEntries[0];			
+					printf("%d dBm\n", pBssEntry->lRssi);
+					fprintf(fp, "%d dBm\n", pBssEntry->lRssi);
+					fflush(fp);
+				} else {
+					printf("N/A\n");
+					fprintf(fp, "N/A\n");
+					fflush(fp);
+				}
 			}
-			else
-			{
-				wprintf(L"RSSI : N/A\n\n");
-			}
-		}		
+			printf("\n");
+			fprintf(fp, "\n");
+			fflush(fp);
+		}
+	}
+
+	if (fp != NULL)	{
+		fclose(fp);
+		fp = NULL;
 	}
 	
-	if (pConnectInfo != NULL) 
-	{
+	if (pConnectInfo != NULL) {
 		WlanFreeMemory(pConnectInfo);
 		pConnectInfo = NULL;
 	}
 
-	if (pBssList != NULL)
-	{
+	if (pBssList != NULL) {
 		WlanFreeMemory(pBssList);
 		pBssList = NULL;
 	}
 
-	if (pAvaList != NULL)
-	{
+	if (pAvaList != NULL) {
 		WlanFreeMemory(pAvaList);
 		pAvaList = NULL;
 	}
 
-	if (pIfList != NULL)
-	{
+	if (pIfList != NULL) {
 		WlanFreeMemory(pIfList);
 		pIfList = NULL;
 	}

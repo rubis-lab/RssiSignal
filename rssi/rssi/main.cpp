b0b87e521067f8cc03d8103c9ad350da986c8ed3
chanheechoi <choi.chanhee@gmail.com>
2015-12-11 16:13:43 +0900

    Mode selector

---
+++ rssi/rssi/main.cpp
@@ -0,0 +1,68 @@
+#include "rssi.h"
+
+void printMenu(void)
+{
+	printf("====== < M E N U > ======\n");
+	printf("1. 1-time measurement\n");
+	printf("2. Continuous measurement\n");
+	printf("3. Stop measurement\n");
+	printf("4. Quit program\n");
+	printf("=========================\n");	
+}
+
+bool bThread;
+
+DWORD WINAPI thRssiFn(LPVOID lpParam)
+{
+	while(bThread) {
+		getRssi();
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	DWORD dwThreadID;
+	HANDLE hThread;
+	
+	bThread = true;
+
+	int nSelect = 0;
+	int nRet = 0;
+	bool bQuit = false;
+	
+	while(1)
+	{
+		printMenu();
+		printf("Input > ");
+		scanf("%d", &nSelect);		
+		switch(nSelect)
+		{
+		case 1:			
+			getRssi();	
+			break;
+		case 2:
+			hThread = CreateThread(NULL, 0, thRssiFn, NULL, 0, &dwThreadID);
+			break;
+		case 3:			
+			bThread = false;
+			WaitForSingleObject(hThread, INFINITE);
+			CloseHandle(hThread);
+			break;
+		case 4:
+			bQuit = true;
+			break;
+		default:
+			printf("Wrong Input!!! (1-4)\n");
+			break;
+		}
+
+		if(bQuit)
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
--- rssi/rssi/rssi.cpp
+++ rssi/rssi/rssi.cpp
@@ -1,20 +1,8 @@
-#include <windows.h>
-#include <wlanapi.h>
-#include <windot11.h> // for DOT11_SSID struct
-#include <ObjBase.h>
-#include <WTypes.h>
+#include "rssi.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#pragma comment(lib, "wlanapi.lib")
-#pragma comment(lib, "ole32.lib")
-
-int main(void)
+int getRssi(void)
 {
 	FILE* fp = NULL;
-	fp = fopen("test.txt", "w");
-	
 	HANDLE hClient = NULL;
 	DWORD dwMaxClient = 2;
 	DWORD dwCurVersion = 0;
@@ -23,15 +11,14 @@ int main(void)
 	int iRet = 0;
 
 	WCHAR GuidString[39] = { 0 };
-		
-	PWLAN_INTERFACE_INFO_LIST pIfList = NULL;	
+
+	PWLAN_INTERFACE_INFO_LIST pIfList = NULL;
 	PWLAN_INTERFACE_INFO pIfInfo = NULL;
 	PWLAN_BSS_LIST pBssList = NULL;
-	PWLAN_BSS_ENTRY pBssEntry = NULL;	
-    PWLAN_AVAILABLE_NETWORK_LIST pAvaList = NULL;
+	PWLAN_BSS_ENTRY pBssEntry = NULL;
+	PWLAN_AVAILABLE_NETWORK_LIST pAvaList = NULL;
 	PWLAN_AVAILABLE_NETWORK pAvaEntry = NULL;
 
-	
 	PWLAN_CONNECTION_ATTRIBUTES pConnectInfo = NULL;
 	DWORD connectInfoSize = sizeof(WLAN_CONNECTION_ATTRIBUTES);
 	WLAN_OPCODE_VALUE_TYPE opCode = wlan_opcode_value_type_invalid;
@@ -39,117 +26,125 @@ int main(void)
 	LONG rssi = 0;
 	DWORD dwSizeRssi = sizeof(rssi);
 
+	time_t ltime;
+	struct tm *today;
+
+	fp = fopen("result.txt", "a");
+
 	// Open a connection to the server
 	dwResult = WlanOpenHandle(dwMaxClient, NULL, &dwCurVersion, &hClient);
 	if (dwResult != ERROR_SUCCESS) {
 		printf("WlanOpenHandle failed with error: %u\n", dwResult);		
-		return 1;		
+		return 0;			
 	}
 
-	// Enumerate all of the wireless LAN interfaces
 	dwResult = WlanEnumInterfaces(hClient, NULL, &pIfList);
 	if (dwResult != ERROR_SUCCESS) {
 		printf("WlanEnumInterfaces failed with error: %u\n", dwResult);
-		return 1;		
+		return 0;		
 	}
-	else
-	{		
-		for (int i = 0; i < (int) pIfList->dwNumberOfItems; i++) {
-			pIfInfo = (WLAN_INTERFACE_INFO *) & pIfList->InterfaceInfo[i];			
-			iRet = StringFromGUID2(pIfInfo->InterfaceGuid, (LPOLESTR) & GuidString, sizeof(GuidString) / sizeof(*GuidString));			
-			if (iRet == 0) {
-				printf("StringFromGUID2 failed\n");
-			}			
-		}
-		
-		while(1) {
-			// Query various parameters of a specified interface
-			dwResult = WlanQueryInterface(hClient,
-				&pIfInfo->InterfaceGuid,
-				wlan_intf_opcode_current_connection,					
-				NULL,
-				&connectInfoSize,
-				(PVOID *) &pConnectInfo,					
-				&opCode);
-			if (dwResult != ERROR_SUCCESS) {
-				printf("WlanQueryInterface failed with error: %u\n", dwResult);
-				return 0;
-			}
 
-			// Request a scan for available networks on the indicated interface
-			dwResult = WlanScan(hClient,
-				&pIfInfo->InterfaceGuid,
-				&pConnectInfo->wlanAssociationAttributes.dot11Ssid,
-				NULL,
-				NULL);
-			if (dwResult != ERROR_SUCCESS) {
-				printf("WlanScan failed with error: %u\n", dwResult);
-				return 0;
-			}		
+	for (int i = 0; i < (int) pIfList->dwNumberOfItems; i++) {
+		pIfInfo = (WLAN_INTERFACE_INFO *) & pIfList->InterfaceInfo[i];			
+		iRet = StringFromGUID2(pIfInfo->InterfaceGuid, (LPOLESTR) & GuidString, sizeof(GuidString) / sizeof(*GuidString));			
+		if (iRet == 0) {
+			printf("StringFromGUID2 failed\n");
+		}			
+	}
 
-			// Retrieves the list of available networks on a wireless LAN interface
-			dwResult = WlanGetAvailableNetworkList(hClient,
-				&pIfInfo->InterfaceGuid,
-				0,
-				NULL,
-				&pAvaList);
-			if (dwResult != ERROR_SUCCESS) {
-				printf("WlanGetAvailableNetworkList failed with error: %u\n", dwResult);
-				return 0;
-			}
+	// Query various parameters of a specified interface
+	dwResult = WlanQueryInterface(hClient,
+		&pIfInfo->InterfaceGuid,
+		wlan_intf_opcode_current_connection,					
+		NULL,
+		&connectInfoSize,
+		(PVOID *) &pConnectInfo,					
+		&opCode);
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanQueryInterface failed with error: %u\n", dwResult);
+		return 0;
+	}
 
-			printf("Number of APs : %lu\n", pAvaList->dwNumberOfItems);
-			fprintf(fp, "Number of APs : %lu\n", pAvaList->dwNumberOfItems);
-			fflush(fp);
-			for(int j = 0; j < pAvaList->dwNumberOfItems; j++)
-			{
-				pAvaEntry = (WLAN_AVAILABLE_NETWORK *) & pAvaList->Network[j];
-				for(int k = 0; k < pAvaEntry->dot11Ssid.uSSIDLength; k++)
-				{
-					printf("%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
-					fprintf(fp, "%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
-					fflush(fp);
-				}
-				printf("\t");
-				fprintf(fp, "\t");
-				fflush(fp);
+	// Request a scan for available networks on the indicated interface
+	dwResult = WlanScan(hClient,
+		&pIfInfo->InterfaceGuid,
+		&pConnectInfo->wlanAssociationAttributes.dot11Ssid,
+		NULL,
+		NULL);
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanScan failed with error: %u\n", dwResult);
+		return 0;
+	}		
 
-				// retrieves a list of the basic service set (BSS) entries of the wireless or networks on a given interface
-				dwResult = WlanGetNetworkBssList(hClient,
-					&pIfInfo->InterfaceGuid,
-					&pAvaEntry->dot11Ssid,
-					dot11_BSS_type_infrastructure,
-					TRUE,
-					NULL,
-					&pBssList);
-				if (dwResult != ERROR_SUCCESS) {
-					printf("WlanGetNetworkBssList failed with error: %u\n", dwResult);
-					return 0;
-				}				
+	// Retrieves the list of available networks on a wireless LAN interface
+	dwResult = WlanGetAvailableNetworkList(hClient,
+		&pIfInfo->InterfaceGuid,
+		0,
+		NULL,
+		&pAvaList);
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanGetAvailableNetworkList failed with error: %u\n", dwResult);
+		return 0;
+	}
 
-				// Get the RSSI value			
-				if (pBssList->dwNumberOfItems > 0) {
-					pBssEntry = &pBssList->wlanBssEntries[0];			
-					printf("%d dBm\n", pBssEntry->lRssi);
-					fprintf(fp, "%d dBm\n", pBssEntry->lRssi);
-					fflush(fp);
-				} else {
-					printf("N/A\n");
-					fprintf(fp, "N/A\n");
-					fflush(fp);
-				}
-			}
-			printf("\n");
-			fprintf(fp, "\n");
+	time(&ltime);
+	today = localtime(&ltime);
+	printf("%04d-%02d-%02d %02d:%02d:%02d\n", today->tm_year + 1900, today->tm_mon + 1, today->tm_mday,
+		today->tm_hour,	today->tm_min, today->tm_sec);
+	fprintf(fp, "%04d-%02d-%02d %02d:%02d:%02d\n", today->tm_year + 1900, today->tm_mon + 1, today->tm_mday,
+		today->tm_hour,	today->tm_min, today->tm_sec);
+	fflush(fp);
+
+	printf("Number of APs : %lu\n", pAvaList->dwNumberOfItems);
+	fprintf(fp, "Number of APs : %lu\n", pAvaList->dwNumberOfItems);
+	fflush(fp);
+	for(int j = 0; j < pAvaList->dwNumberOfItems; j++)
+	{
+		pAvaEntry = (WLAN_AVAILABLE_NETWORK *) & pAvaList->Network[j];
+		for(int k = 0; k < pAvaEntry->dot11Ssid.uSSIDLength; k++)
+		{
+			printf("%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
+			fprintf(fp, "%c", (int) pAvaEntry->dot11Ssid.ucSSID[k]);
 			fflush(fp);
 		}
+		printf("\t");
+		fprintf(fp, "\t");
+		fflush(fp);
+
+		// retrieves a list of the basic service set (BSS) entries of the wireless or networks on a given interface
+		dwResult = WlanGetNetworkBssList(hClient,
+			&pIfInfo->InterfaceGuid,
+			&pAvaEntry->dot11Ssid,
+			dot11_BSS_type_infrastructure,
+			TRUE,
+			NULL,
+			&pBssList);
+		if (dwResult != ERROR_SUCCESS) {
+			printf("WlanGetNetworkBssList failed with error: %u\n", dwResult);
+			return 0;
+		}				
+
+		// Get the RSSI value			
+		if (pBssList->dwNumberOfItems > 0) {
+			pBssEntry = &pBssList->wlanBssEntries[0];			
+			printf("%d dBm\n", pBssEntry->lRssi);
+			fprintf(fp, "%d dBm\n", pBssEntry->lRssi);
+			fflush(fp);
+		} else {
+			printf("N/A\n");
+			fprintf(fp, "N/A\n");
+			fflush(fp);
+		}
 	}
+	printf("\n");
+	fprintf(fp, "\n");
+	fflush(fp);
 
 	if (fp != NULL)	{
 		fclose(fp);
 		fp = NULL;
 	}
-	
+
 	if (pConnectInfo != NULL) {
 		WlanFreeMemory(pConnectInfo);
 		pConnectInfo = NULL;
@@ -170,5 +165,11 @@ int main(void)
 		pIfList = NULL;
 	}
 
-	return 0;
+	dwResult = WlanCloseHandle(hClient, NULL);
+	if (dwResult != ERROR_SUCCESS) {
+		printf("WlanCloseHandle failed with error : %u\n", dwResult);
+		return 0;
+	}
+
+	return 1;
 }
